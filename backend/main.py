9945f08601d977d948a6c02bbbe9de598512c18d
virchau13 <virchau13@hexular.net>
2022-01-08 01:54:56 +0800

    backend initial prototype

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,16 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, request, session
 from flask_socketio import SocketIO, send, emit
 import numpy as np
 import spacy
 from scipy.spatial import distance
 import random
-import string
 import os
 from uuid import uuid4
+import time
+import math
 
 nlp = spacy.load('en_core_web_lg')
 word_ids = [word_id for word_id in nlp.vocab.vectors.keys() if (lambda word: word.isalpha() and word.islower())(nlp.vocab[word_id].text)]
@@ -46,21 +50,44 @@ def closest_n_words(inp_word, n):
 def random_code() -> int:
     return random.randint(100_000, 999_999)
 
+# Random word from the NLP dataset.
+def random_word() -> str:
+    word_id = random.choice(word_ids)
+    return nlp.vocab[word_id].text
+
+def dist_to_score(distance: float) -> int:
+    return int(math.ceil(np.exp(-distance) * 100))
+
+def calc_score(word: str, guess: (str, str, str)) -> int:
+    dist = vec_dist(wv(word), wv(guess[0]) - wv(guess[1]) + wv(guess[2]))
+    return dist_to_score(dist)
+
 class Room:
     code: int
     player_ids: list[str]
     playing: list[str]
     ready: list[str]
+    roundNumber: int
+    word: str
+    endTime: int
+    submissions: dict[str, (str, str, str)]
+    roundScores: dict[str, int] = {}
 
     def __init__(self, creator_id: str):
         self.code = random_code()
         self.player_ids = [creator_id]
+        self.playing = []
+        self.ready = []
+        self.roundNumber = 0
 
     def join_room(self, player_id: str):
         self.player_ids.append(player_id)
 
     def is_playing(self) -> bool:
         return len(self.playing) > 0
+
+    def sid_is_playing(self, sid: str) -> bool:
+        return sid in self.playing
 
     def get_info(self) -> dict[str, any]:
         return {
@@ -79,8 +106,33 @@ class Room:
             if already_present:
                 self.ready.remove(sid)
 
-    def game_should_start(self):
+    def round_should_start(self):
         return len(self.ready) > len(self.playing)/2
+
+    def start_round(self) -> dict[str, any]: 
+        self.roundNumber += 1
+        if self.roundNumber == 0:
+            self.roundScores = { k:0 for k in self.playing }
+        self.word = random_word()
+        ROUND_TIME = 30 # seconds
+        self.endTime = int(time.time()) + ROUND_TIME
+        self.submissions = {}
+        return {
+            "roundNumber": self.roundNumber,
+            "word": self.word,
+            "timeLeft": ROUND_TIME
+        }
+
+    def end_round(self) -> dict[str, any]:
+        # calculate score
+        for sid in self.playing:
+            if sid in self.submissions:
+                self.roundScores[sid] += calc_score(self.word, self.submissions[sid])
+        return {
+            "roundNumber": self.roundNumber,
+            "guesses": { sids_to_usernames[sid]:guess for sid,guess in self.submissions.items() },
+            "playerScores": { sids_to_usernames[sid]:score for sid,score in self.roundScores.items() }
+        }
 
 sids_to_usernames: dict[str, str] = {}
 def idsToNames(sid_arr):
@@ -142,10 +194,39 @@ def socket_connect():
 def player_status_update(data):
     if data is None or 'ready' not in data or not isinstance(data['ready'], bool):
         return { 'error', 'bad body' }, 400
-    rooms[session['code']].set_ready(get_sid())
-    emit('room-info', rooms[session['code']].get_info())
-    if rooms[session['code']].game_should_start():
-        pass # TODO
+    room = rooms[session['code']]
+    room.set_ready(get_sid())
+    emit('room-info', room.get_info())
+    if room.round_should_start():
+        socketio.sleep(5)
+        if room.round_should_start(): # someone may have unreadied in the meantime
+            # actual round logic, XXX this is probably not the best place to put it
+            while room.roundNumber < 5:
+                emit('round-start', room.start_round())
+                socketio.sleep(room.endTime - int(time.time()))
+                emit('round-end', room.end_round())
+                socketio.sleep(7)
+
+@socketio.on('round-submit')
+def player_round_submit(data):
+    if data is None or 'words' not in data or not isinstance(data['words'], list) or len(data['words']) != 3 or not all(isinstance(word, str) for word in data['words']):
+        return { 'error': 'bad body' }, 400
+    words = data['words']
+    room = rooms[session['code']]
+    sid = get_sid()
+    if not room.sid_is_playing(sid):
+        return { 'error': 'Round has not started yet' }, 400
+    if sid in room.submissions:
+        return { 'error': 'You have already submitted for this round' }, 400
+    # filter non-alphabet
+    if not words.isalpha():
+        return { 'error': 'Cannot have non-alphabetical words' }, 400
+    words = [w.lower() for w in words]
+    # filter duplicates
+    if any(words.count(w) > 1 for w in words):
+        return { 'error': 'Cannot duplicate words' }, 400
+    room.submissions[sid] = tuple(words)
+    emit('round-update', { 'username': session['username'] })
 
 if __name__ == '__main__':
     socketio.run(app)
